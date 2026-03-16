44409caca83bebf937de738025dec737537c0d4a
MAAUN/24/CBS/0112 <izainababdullahi123@gmail.com>
2026-03-16 12:22:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from models import Business
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='Assignment4/templates')
 
 businesses = []
 recent_stack = []   # Stack for recently added businesses
